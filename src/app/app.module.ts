8662629b47c3c7d09eb446d336f863f79296a709
ronakgala18 <ronakgala18@gmail.com>
2023-04-09 19:55:34 -0400

    routes added along with graphql connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS, } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ApolloModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+        provide: APOLLO_OPTIONS,
+        useFactory(httpLink: HttpLink) {
+            return {
+                cache: new InMemoryCache(),
+                link: httpLink.create({
+                    uri: "https://troubled-clothes-calf.cyclic.app/graphql",
+                }),
+            };
+        },
+        deps: [HttpLink],
+    },
+],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
